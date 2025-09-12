34f7b370348fa9404a85e2d2e98aab572a3f7853
olanrewaju24 <mercyfol.lanre@gmail.com>
2025-09-12 11:43:57 +0100

    Changes in cart.jsx and cartcontext.jsx

--- src/state/CartContext.jsx
+++ src/state/CartContext.jsx
@@ -1,7 +1,7 @@
 // src/state/CartContext.jsx
 import React, { createContext, useContext, useState } from "react";
 
-const CartContext = createContext();
+export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
